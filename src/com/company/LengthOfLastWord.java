5327e79ca4cd554c354d52224ec7ec710b4a4448
ksyundelb <112008807+ksyundelb@users.noreply.github.com>
2022-09-15 17:46:34 +0200

    Update LengthOfLastWord.java

--- src/com/company/LengthOfLastWord.java
+++ src/com/company/LengthOfLastWord.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
         int length = 0;
-
-        // We are looking for the last word so let's go backward
         for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ') { // a letter is found so count
+            if (s.charAt(i) != ' ') { 
                 length++;
-            } else {  // it's a white space instead
-                //  Did we already started to count a word ? Yes so we found the last word
+            } else {  
                 if (length > 0) return length;
             }
         }
