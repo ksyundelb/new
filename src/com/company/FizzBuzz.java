0ac62c29f1f71ef5d37f6151eb2934f7c0ce43a8
Ksenia Shakhina <ksenia_shakhina@epam.com>
2022-09-16 14:09:27 +0200

    recheck

--- src/com/company/FizzBuzz.java
+++ src/com/company/FizzBuzz.java
@@ -1,2 +1,23 @@
-package com.company;public class FizzBuzz {
+package com.company;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    void fizzbuzz(int x) {
+        if (x % 5 == 0 && x % 3 == 0) {
+            System.out.println("fizz-buzz");
+        } else if (x % 5 == 0) {
+            System.out.println("buzz");
+        } else if (x % 3 == 0) {
+            System.out.println("fizz");
+        }
+    }
 }
+    class FizzBuzzTest {
+        public static void main(String[] args) {
+            FizzBuzz number = new FizzBuzz();
+            Scanner scanner = new Scanner(System.in);
+            int newNumber = scanner.nextInt();
+            number.fizzbuzz(newNumber);
+        }
+    }
